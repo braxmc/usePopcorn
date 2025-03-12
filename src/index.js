b07c380894148909bf62fdd4caf37cbab0f3ff48
Braxton McClellan <thebraxmc@gmail.com>
2025-03-11 23:48:07 -0600

    created a star rating component using useState and hover events

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import StarRating from './components/StarRating.jsx';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} />
   </React.StrictMode>
 );
